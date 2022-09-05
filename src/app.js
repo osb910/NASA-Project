cac5439fc4c6837a5d343e2ffc868cd6ebaa81cb
Omar Shareef Abdul-Bar <omarshdev@gmail.com>
2022-09-05 08:23:37 +0200

    Fixing cors

--- src/app.js
+++ src/app.js
@@ -7,35 +7,40 @@ const api = require('./routes/api');
 
 const app = express();
 
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+
 app.use(
   cors({
     origin: 'https://nasa-mission-control-omarsh.vercel.app/',
   })
 );
-origin: 'http://localhost:3000',
-  // origin: '*',
+// origin: 'http://localhost:3000',
+// origin: '*',
 
-  // app.use((req, res, next) => {
-  //   //   res.header(
-  //   //     'Access-Control-Allow-Headers',
-  //   //     'Origin, X-Requested-With, Content-Type, Accept'
-  //   //   );
-  //   res.setHeader('Access-Control-Allow-Credentials', true);
-  //   res.setHeader('Access-Control-Allow-Origin', '*');
-  //   // another common pattern
-  //   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  //   res.setHeader(
-  //     'Access-Control-Allow-Methods',
-  //     'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-  //   );
-  //   res.setHeader(
-  //     'Access-Control-Allow-Headers',
-  //     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  //   );
-  //   next();
-  // });
+// app.use((req, res, next) => {
+//   //   res.header(
+//   //     'Access-Control-Allow-Headers',
+//   //     'Origin, X-Requested-With, Content-Type, Accept'
+//   //   );
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   // another common pattern
+//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   );
+//   next();
+// });
 
-  app.use(morgan('combined'));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
