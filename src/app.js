51e72c78c307ad5c3774e3edcc4dc9e50c7de766
Omar Shareef Abdul-Bar <omarshdev@gmail.com>
2022-09-05 11:26:19 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/v1', api);
+app.get('/v1', api);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
