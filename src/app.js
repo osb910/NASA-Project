86a7b252c29bb868ab3a2b927e53d27d42e16aac
Omar Shareef Abdul-Bar <omarshdev@gmail.com>
2022-09-05 04:17:17 +0200

    Fixing cors

--- src/app.js
+++ src/app.js
@@ -7,11 +7,20 @@ const api = require('./routes/api');
 
 const app = express();
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use(morgan('combined'));
 app.use(express.json());
